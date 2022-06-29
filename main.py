ff85be28a03a64adfa30b7d6df6c5938da4a903f
Marrlenns <marlen.ormonbaev05@gmail.com>
2022-06-30 00:09:19 +0600

    marselge jiberotam

---
+++ main.py
@@ -0,0 +1,31 @@
+class Player:
+    def __init__(self, name, bet):
+        self.name = name
+        self.bet = bet
+        self.point = 0
+
+    def hit(self):
+        pass
+
+    def stand(self):
+        pass
+
+    def split(self):
+        pass
+
+    def double(self):
+        pass
+
+    def surrender(self):
+        pass
+
+class Diller:
+    pass
+
+class Deck:
+    def __str__(self):
+        pass
+
+class BlackJack(Diller):
+    pass
+
